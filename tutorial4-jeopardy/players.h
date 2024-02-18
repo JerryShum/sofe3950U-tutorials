1bc4e27ed3bb032e578d5c4abb96de74e981e00f
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-02-18 18:08:10 -0500

    Add files via upload

---
+++ tutorial4-jeopardy/players.h
@@ -0,0 +1,28 @@
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#ifndef PLAYERS_H_
+#define PLAYERS_H_
+
+#include <stdbool.h>
+
+#define MAX_LEN 256
+
+// Player struct for each player
+typedef struct {
+    char name[MAX_LEN];
+    int score;
+} player;
+
+// Returns true if the player name matches one of the existing players
+extern bool player_exists(player *players, int num_players, char *name);
+
+// Go through the list of players and update the score for the 
+// player given their name
+extern void update_score(player *players, int num_players, char *name, int score);
+
+#endif /* PLAYERS_H_ */
